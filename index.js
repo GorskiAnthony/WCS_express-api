02a8de98d81a92d40030178e4d8365493ad38e81
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-04-11 14:51:08 +0200

    init server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const PORT = 5050;
+const app = express();
+
+app.listen(PORT, (err) => {
+	if (err) {
+		console.error("erreur du server: ", err);
+	} else {
+		console.log(`Le server run sur le port: ${PORT}`);
+	}
+});
